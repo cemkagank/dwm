ea972725430279504d8cfb96ae1335f910acbaf1
Cem Kağan <cemkagankayili@hotmail.com>
2022-02-09 22:46:19 +0300

    updated colorw

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#ed601c";
+static const char col_cyan[]        = "#11c21d";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
